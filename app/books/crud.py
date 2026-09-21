d3c6185824058e8de806032fa74327134b7a6f9d
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2026-09-21 17:36:25 +0300

    test pr review feature

--- app/books/crud.py
+++ app/books/crud.py
@@ -136,6 +136,8 @@ async def update_book(db: Session, book: Book, data: dict) -> Book:
 
     if not book:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=NOT_FOUND_ERROR)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=NOT_FOUND_ERROR)
 
     if "writer_ids" in data:
         writers = get_writers_by_ids(db, data.get("writer_ids"))
